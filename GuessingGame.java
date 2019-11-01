5d29a94c160b3d1457b5b2d7944755f6fd15ef46
Anners <anners@mailzorz.com>
2019-11-02 00:00:37 +0100

    lade till konstruktor + parametercheck för konstruktor samt klasser som testar båda

---
+++ GuessingGame.java
@@ -0,0 +1,9 @@
+public class GuessingGame {
+
+	public static void main(String[] args) {
+		 Guesser g1 = new Guesser(0, 1000);
+		 g1.start();
+	}
+
+}
+
---
+++ TestGuesser.java
@@ -0,0 +1,7 @@
+public class TestGuesser {
+
+	public static void main(String[] args) {
+		Guesser g2 = new Guesser(40, 30);
+			System.out.println(g2);
+	}
+}
